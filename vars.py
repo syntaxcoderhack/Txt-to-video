2783e922138dafb1b6f229e96d5dab8c9427a639
nikhilsaini098 <nikhilsaini01012001@gmail.com>
2024-12-23 21:27:41 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -2,9 +2,9 @@
 # Add your details here and then deploy by clicking on HEROKU Deploy button
 import os
 
-API_ID    = os.environ.get("API_ID", "24495656")
-API_HASH  = os.environ.get("API_HASH", "61afcf68c6429714dd18acd07f246571")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7847799213:AAGhkL3RhRedZpopQDNBIrIdfFc7kkQMNxw") 
+API_ID    = os.environ.get("API_ID", "28094744")
+API_HASH  = os.environ.get("API_HASH", "a75af4285edc7747c57bb19147ca0b9b")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7680621096:AAH9ON_ca_aJU9iVY63X6wvXLR4v0Q-hlQ8") 
 
 #WEBHOOK = True  # Don't change this
 #PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
