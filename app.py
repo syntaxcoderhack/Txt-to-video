b32e0ab9e793638cdb299284cd1994e010013cc9
nikhilsaini098 <nikhilsaini01012001@gmail.com>
2024-12-27 09:13:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def hello_world():
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" type="text/css" href="https://sudor2spr.github.io/Documentation/assets/style.css">
     <titleSudoR2spr Repository</title>
-	<link rel="icon" type="image/x-icon" href="https://raw.githubusercontent.com/SudoR2spr/SudoR2spr/main/assets/angel-op/Angel-ji.png">
+	<link rel="icon" type="image/x-icon" href="">
 
 </head>
 
@@ -31,9 +31,9 @@ def hello_world():
     </div>
 	<br></br><br></br><br></br>
 	<footer class="bg-dark text-white text-center py-3 mt-5">
-	<center><img loading="lazy" class="object-none object-center" src="https://graph.org/file/548b8b73c35af202bfdac.png" width="60" height="60">
-        Powered By 𝐖𝐎𝐎𝐃𝐜𝐫𝐚𝐟𝐭 
-		<img loading="lazy" class="object-none object-center" src="https://graph.org/file/548b8b73c35af202bfdac.png" width="60" height="60">
+	<center><img loading="lazy" class="object-none object-center" src="" width="60" height="60">
+        Powered By NIKHIL 
+		<img loading="lazy" class="object-none object-center" src="" width="60" height="60">
 		<div class="footer__copyright">
             <p class="footer__copyright-info">
                 © 2024 Video Downloader. All rights reserved.
